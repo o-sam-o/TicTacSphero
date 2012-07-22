bb47881ec272eed50411f4b05bc90c2b950b6a19
David Foster <davidfstr@gmail.com>
2012-07-21 17:53:22 -0700

    Reorganize GameView methods.

--- TicTacSphero/src/com/tictac/sphero/view/GameView.java
+++ TicTacSphero/src/com/tictac/sphero/view/GameView.java
@@ -18,8 +18,7 @@ import com.tictac.sphero.game.Game;
 import com.tictac.sphero.game.Player;
 
 public class GameView extends View {
-
-	public final static int BOARD_SIZE = 3;
+	private final static int BOARD_SIZE = 3;
 	
 	private Game game;
 	private Paint paint;
@@ -41,6 +40,8 @@ public class GameView extends View {
         xOrbImg = BitmapFactory.decodeResource(getResources(), R.drawable.x_orb);
         yOrbImg = BitmapFactory.decodeResource(getResources(), R.drawable.y_orb);
 	}
+	
+	// === Draw ===
 
 	@Override
 	protected void onDraw(Canvas canvas) {
@@ -49,22 +50,94 @@ public class GameView extends View {
 		
   	    for (int x=0; x<3; x++) {
   	    	for (int y=0; y<3; y++) {
-  	    		drawMove(canvas, x, y, game.get(x, y));
+  	    		drawOrb(canvas, x, y, game.get(x, y));
   	    	}
   	    }
   	    
 	    super.onDraw(canvas);
 	}
 	
-	/** Copies the state of Game to SpheroGrid. */
-	private void updateSpheroGrid() {
-		for (int x=0; x<3; x++) {
-  	    	for (int y=0; y<3; y++) {
-  	  	    	// TODO: spheroGrid.set(x, y, game.get(x, y));
-  	    	}
-  	    }
+	private void drawBoardLines(Canvas canvas) {
+		int xs = this.getWidth() / BOARD_SIZE;
+		int ys = this.getHeight() / BOARD_SIZE;
+		for (int i = 0; i <= BOARD_SIZE; i++) {
+			canvas.drawLine(xs * i, 0, xs * i, this.getHeight(), paint);
+		}
+		for (int i = 0; i <= BOARD_SIZE; i++) {
+			canvas.drawLine(0, ys * i, this.getWidth(), ys * i, paint);
+		}
 	}
+	
+	private void drawOrb(Canvas canvas, int xCell, int yCell, Player player) {
+		if (player == Player.NONE) {
+			return;
+		}
+		
+		Log.d("TTS", "draw move (" + xCell + "," + yCell + ") for " + player);
+		Point pixelLocation = convertGridToPixelLocation(xCell, yCell);
+		canvas.drawBitmap(getPlayerOrbImage(player), pixelLocation.x, pixelLocation.y, new Paint());
+	}
+	
+	private Point convertGridToPixelLocation(int xCell, int yCell) {
+		Point point = new Point();
 
+		int cellWidth = (getWidth() / BOARD_SIZE);
+		int cellHeight = (getHeight() / BOARD_SIZE);
+		
+		int imgWidth = xOrbImg.getWidth();
+		int imgHeight = xOrbImg.getHeight();
+		
+		int xOffset = (cellWidth / 2) - (imgWidth / 2);
+		int yOffset = (cellHeight / 2) - (imgHeight / 2);
+		
+		int cZeroX = cellWidth * xCell;
+		int cZeroY = cellHeight * yCell;
+		
+		point.x = cZeroX + xOffset;
+		point.y = cZeroY + yOffset;
+		return point;
+	}
+	
+	private Bitmap getPlayerOrbImage(Player player) {
+		switch(player) {
+			case X:
+				return xOrbImg;
+			case O:
+				return yOrbImg;
+			default:
+				throw new RuntimeException("Unknown player type " + player);
+		}
+	}
+	
+	// === Touch ===
+	
+	@Override
+    public boolean onTouchEvent(MotionEvent event) {
+		// Only process "tap up" events
+		if (event.getAction() != MotionEvent.ACTION_UP) {
+			return true;	// needed so that ACTION_UP is sent later
+		}
+		
+		// Ignore taps when game is over
+		if (game.isGameOver()) {
+			return true;
+		}
+		
+		int x = (int) (event.getX() / (this.getWidth() / BOARD_SIZE));
+        int y = (int) (event.getY() / (this.getHeight() / BOARD_SIZE));
+        Log.d("TTS", String.format("Click on (%s, %s)", x, y));
+        
+        game.makeMove(x, y);
+        this.gameDidChange();
+        
+        if (game.isGameOver()) {
+            showGameOverDialog();
+  	    }
+        
+        super.onTouchEvent(event);
+        return true;
+    }
+	
 	public void showGameOverDialog() {
 		Log.i("TTS", "Winner is " + game.getWinner());
 		
@@ -95,88 +168,19 @@ public class GameView extends View {
 		dialog.show();
 	}
 	
-	@Override
-    public boolean onTouchEvent(MotionEvent event) {
-		// Only process "tap up" events
-		if (event.getAction() != MotionEvent.ACTION_UP) {
-			return true;	// needed so that ACTION_UP is sent later
-		}
-		
-		// Ignore taps when game is over
-		if (game.isGameOver()) {
-			return true;
-		}
-		
-		int x = (int) (event.getX() / (this.getWidth() / BOARD_SIZE));
-        int y = (int) (event.getY() / (this.getHeight() / BOARD_SIZE));
-        Log.d("TTS", String.format("Click on (%s, %s)", x, y));
-        
-        game.makeMove(x, y);
-        this.gameDidChange();
-        
-        if (game.isGameOver()) {
-            showGameOverDialog();
-  	    }
-        
-        super.onTouchEvent(event);
-        return true;
-    }
+	// === Dependencies ===
 	
 	private void gameDidChange() {
 		this.invalidate();			// repaint self
         this.updateSpheroGrid();
 	}
 	
-	private void drawMove(Canvas canvas, int xCell, int yCell, Player player) {
-		if (player == Player.NONE) {
-			return;
-		}
-		
-		Log.d("TTS", "draw move (" + xCell + "," + yCell + ") for " + player);
-		Point imgLocation = getImgLocationFor(xCell, yCell);
-		canvas.drawBitmap(getPlayerOrb(player), imgLocation.x, imgLocation.y, new Paint());
+	/** Copies the state of Game to SpheroGrid. */
+	private void updateSpheroGrid() {
+		for (int x=0; x<3; x++) {
+  	    	for (int y=0; y<3; y++) {
+  	  	    	// TODO: spheroGrid.set(x, y, game.get(x, y));
+  	    	}
+  	    }
 	}
-	
-	private Point getImgLocationFor(int xCell, int yCell) {
-		Point point = new Point();
-
-		int cellWidth = (getWidth() / BOARD_SIZE);
-		int cellHeight = (getHeight() / BOARD_SIZE);
-		
-		int imgWidth = xOrbImg.getWidth();
-		int imgHeight = xOrbImg.getHeight();
-		
-		int xOffset = (cellWidth / 2) - (imgWidth / 2);
-		int yOffset = (cellHeight / 2) - (imgHeight / 2);
-		
-		int cZeroX = cellWidth * xCell;
-		int cZeroY = cellHeight * yCell;
-		
-		point.x = cZeroX + xOffset;
-		point.y = cZeroY + yOffset;
-		return point;
-	}
-	
-	private Bitmap getPlayerOrb(Player player) {
-		switch(player) {
-			case X:
-				return xOrbImg;
-			case O:
-				return yOrbImg;
-			default:
-				throw new RuntimeException("Unknown player type " + player);
-		}
-	}
-	
-	private void drawBoardLines(Canvas canvas) {
-		int xs = this.getWidth() / BOARD_SIZE;
-		int ys = this.getHeight() / BOARD_SIZE;
-		for (int i = 0; i <= BOARD_SIZE; i++) {
-			canvas.drawLine(xs * i, 0, xs * i, this.getHeight(), paint);
-		}
-		for (int i = 0; i <= BOARD_SIZE; i++) {
-			canvas.drawLine(0, ys * i, this.getWidth(), ys * i, paint);
-		}
-	}
-	
 }
